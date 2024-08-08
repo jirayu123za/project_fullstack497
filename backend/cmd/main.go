c01a2e7a74d85baf83d59e55c9e61b8a8b0c2acb
jirayu <jirayu004za@gmail.com>
2024-08-08 19:14:44 +0700

    connect postgres success

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
@@ -18,6 +22,17 @@ func main() {
 
 	// Init fiber server
 	app := fiber.New()
+
+	dsn := os.Getenv("DATABASE_DSN")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+
+	}
+
+	_, err = ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Init routes
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -33,4 +48,29 @@ func main() {
 		log.Fatal("Failed to start server: ", err)
 	}
 	fmt.Println("Server is running on port: ", port)
+}
+
+func ConnectPostgres(dsn string) (*gorm.DB, error) {
+	// Config gorm logger
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connected to database successfully")
+
+	// Migration
+
+	return db, err
 }
