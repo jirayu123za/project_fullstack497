f00484c6125b132babf9136f769ffa9cf1e7b4cc
jirayu <jirayu004za@gmail.com>
2024-08-27 23:52:39 +0700

    init UserRepository

--- backend/internal/core/repositories/user_repository.go
+++ backend/internal/core/repositories/user_repository.go
@@ -1,1 +1,16 @@
 package repositories
+
+import (
+	"backend_fullstack/internal/models"
+
+	"github.com/google/uuid"
+)
+
+// Secondary ports
+type UserRepository interface {
+	Register(user *models.User) error
+	FindUserByID(userID uuid.UUID) (*models.User, error)
+	FindUsers() ([]*models.User, error)
+	ModifyUser(user *models.User) error
+	RemoveUser(user *models.User) error
+}
