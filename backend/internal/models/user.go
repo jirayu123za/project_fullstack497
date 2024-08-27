a66d248c5587d8da094c733634bc0a919fad1ff7
jirayu <jirayu004za@gmail.com>
2024-08-27 23:46:52 +0700

    update User model & generate UUID Before create Users tb

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -3,17 +3,18 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	UserID          string `gorm:"primaryKey;autoIncrement"`
-	GroupID         int    `gorm:"not null"`
-	UserName        string `gorm:"type:varchar(50);not null"`
-	Password        string `gorm:"type:varchar(255);not null"`
-	FirstName       string `gorm:"type:varchar(50)"`
-	LastName        string `gorm:"type:varchar(50)"`
-	Email           string `gorm:"type:varchar(50);not null"`
+	UserID          uuid.UUID `gorm:"primaryKey" json:"user_id"`
+	GroupID         uint      `gorm:"not null" json:"group_id"`
+	UserName        string    `gorm:"type:varchar(50);not null" json:"user_name"`
+	Password        string    `gorm:"type:varchar(255);not null" json:"password"`
+	FirstName       string    `gorm:"type:varchar(50)" json:"first_name"`
+	LastName        string    `gorm:"type:varchar(50)" json:"last_name"`
+	Email           string    `gorm:"type:varchar(50);not null" json:"email"`
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 	DeletedAt       gorm.DeletedAt   `gorm:"index"`
@@ -21,4 +22,11 @@ type User struct {
 	InstructorLists []InstructorList `gorm:"foreignKey:UserID"`
 	Submissions     []Submission     `gorm:"foreignKey:UserID"`
 	Uploads         []Upload         `gorm:"foreignKey:UserID"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if user.UserID == uuid.Nil {
+		user.UserID = uuid.New()
+	}
+	return
 }
