2f9be2ddda98284d2e683be8c0dec08d4fc0bdbd
jirayu <jirayu004za@gmail.com>
2024-08-27 23:49:02 +0700

    update Upload model, change UserID string to uuid.UUID

--- backend/internal/models/upload.go
+++ backend/internal/models/upload.go
@@ -3,13 +3,14 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Upload struct {
-	UploadID         string `gorm:"primaryKey;autoIncrement"`
-	UserID           string `gorm:"not null"`
-	AssignmentFileID string `gorm:"not null"`
+	UploadID         string    `gorm:"primaryKey;autoIncrement"`
+	UserID           uuid.UUID `gorm:"not null"`
+	AssignmentFileID string    `gorm:"not null"`
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
 	DeletedAt        gorm.DeletedAt `gorm:"index"`
