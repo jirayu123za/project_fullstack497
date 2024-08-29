f5d6fb64e074e69918e06ad8590bc72067c94451
jirayu <jirayu004za@gmail.com>
2024-08-29 18:35:34 +0700

    del func GoogleLogin, GoogleCallback because will impl with hexagonal

--- backend/internal/adapters/http_user_adapter.go
+++ backend/internal/adapters/http_user_adapter.go
@@ -1,66 +1,12 @@
 package adapters
 
 import (
-	"backend_fullstack/internal/adapters/auth"
 	"backend_fullstack/internal/core/services"
-	"backend_fullstack/internal/core/utils"
 	"backend_fullstack/internal/models"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
-
-func GoogleLogin(c *fiber.Ctx) error {
-	state, err := utils.GenerateRandomState(32)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Failed to generate random state",
-			"error":   err,
-		})
-	}
-
-	c.Cookie(&fiber.Cookie{
-		Name:  "oauth_state",
-		Value: state,
-	})
-
-	loginURL := auth.GetGoogleLoginURL(state)
-
-	return c.Redirect(loginURL)
-}
-
-func GoogleCallback(c *fiber.Ctx) error {
-	code := c.Query("code")
-	state := c.Query("state")
-
-	if !utils.ValidateState(c, state) {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid state parameter",
-		})
-	}
-
-	token, err := auth.GetGoogleToken(code)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Failed to exchange code for token",
-			"error":   err,
-		})
-	}
-
-	userInfo, err := auth.GetGoogleUserInfo(token.AccessToken)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Failed to get user info",
-			"error":   err,
-		})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "User is logged in by google OAuth2",
-		"token":   token,
-		"user":    userInfo,
-	})
-}
 
 // Primary adapter
 type HttpUserHandler struct {
