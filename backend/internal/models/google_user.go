733c1a7312af9123e0bdad90400b82a05e412e9a
jirayu <jirayu004za@gmail.com>
2024-08-29 18:36:08 +0700

    init model GoogleUserInfo

---
+++ backend/internal/models/google_user.go
@@ -0,0 +1,8 @@
+package models
+
+type GoogleUserInfo struct {
+	Email     string `json:"email"`
+	Name      string `json:"name"`
+	GivenName string `json:"given_name"`
+	Picture   string `json:"picture"`
+}
