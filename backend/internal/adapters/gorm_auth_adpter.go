af340c895fa7116eadcfd66f7f47909f71f5c30f
jirayu <jirayu004za@gmail.com>
2024-08-29 23:22:27 +0700

    complete AuthRepository

---
+++ backend/internal/adapters/gorm_auth_adpter.go
@@ -0,0 +1,40 @@
+package adapters
+
+import (
+	"backend_fullstack/internal/models"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type AuthRepository struct {
+	db *gorm.DB
+}
+
+func NewAuthRepository(db *gorm.DB) *AuthRepository {
+	return &AuthRepository{
+		db: db,
+	}
+}
+
+func (r *AuthRepository) AuthenticateUser(username string, password string) (string, error) {
+	// Implement the logic to authenticate the user
+	var selectedUser models.User
+	if result := r.db.Where("user_name = ?", username).First(&selectedUser); result.Error != nil {
+		return "", result.Error
+	}
+
+	// then compare password of before user
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(selectedUser.Password),
+		[]byte(password),
+	)
+	if err != nil {
+		return "", errors.New("invalid credentials")
+	}
+
+	return selectedUser.UserID.String(), nil
+}
+
+// func (r *AuthRepository) DeleteJWTToken(token string) error { return nil }
