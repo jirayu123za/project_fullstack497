03f56217c2a3de6dda6c49e3416e776382d6591e
jirayu <jirayu004za@gmail.com>
2024-08-29 23:20:11 +0700

    complete HttpAuthHandler

---
+++ backend/internal/adapters/http_auth_adpter.go
@@ -0,0 +1,50 @@
+package adapters
+
+import (
+	"backend_fullstack/internal/core/services"
+	"backend_fullstack/internal/models"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// Primary adapter
+type HttpAuthHandler struct {
+	services services.AuthService
+}
+
+func NewHttpAuthHandler(services services.AuthService) *HttpAuthHandler {
+	return &HttpAuthHandler{
+		services: services,
+	}
+}
+
+func (h *HttpAuthHandler) Login(c *fiber.Ctx) error {
+	var loginUser models.User
+	if err := c.BodyParser(&loginUser); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Failed to parse request body",
+			"error":   err.Error(),
+		})
+	}
+
+	token, err := h.services.Login(loginUser.UserName, loginUser.Password)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Failed to login",
+			"error":   err.Error(),
+		})
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "jwt-token",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 1),
+		HTTPOnly: true,
+	})
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Login successful",
+		"token":   token,
+	})
+}
