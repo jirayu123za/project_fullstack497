e229816da4d120b497b0b8e44850d72e4775e8e2
jirayu <jirayu004za@gmail.com>
2024-08-29 23:51:41 +0700

    add DeleteJWTToken

--- backend/internal/core/repositories/auth_repository.go
+++ backend/internal/core/repositories/auth_repository.go
@@ -2,5 +2,5 @@ package repositories
 
 type AuthRepository interface {
 	AuthenticateUser(username string, password string) (string, error)
-	//DeleteJWTToken(token string) error
+	DeleteJWTToken(token string) error
 }
