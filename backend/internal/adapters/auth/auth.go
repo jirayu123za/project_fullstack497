f790f32baba4db6acfd4303c61a44827dfd1560e
jirayu <jirayu004za@gmail.com>
2024-08-29 18:31:27 +0700

    update InitializeGoogleOAuth

--- backend/internal/adapters/auth/auth.go
+++ backend/internal/adapters/auth/auth.go
@@ -1,11 +1,7 @@
 package auth
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -17,16 +13,9 @@ type Config struct {
 	GoogleLoginConfig oauth2.Config
 }
 
-type GoogleUserInfo struct {
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	GivenName string `json:"given_name"`
-	Picture   string `json:"picture"`
-}
-
 var AppConfig Config
 
-func GoogleOauth() oauth2.Config {
+func InitializeGoogleOAuth() oauth2.Config {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file: ", err)
@@ -45,43 +34,4 @@ func GoogleOauth() oauth2.Config {
 		Endpoint:     google.Endpoint,
 	}
 	return AppConfig.GoogleLoginConfig
-}
-
-func GetGoogleLoginURL(state string) string {
-	return AppConfig.GoogleLoginConfig.AuthCodeURL(state)
-}
-
-func GetGoogleToken(code string) (*oauth2.Token, error) {
-	token, err := AppConfig.GoogleLoginConfig.Exchange(context.Background(), code)
-	if err != nil {
-		return nil, err
-	}
-	return token, nil
-}
-
-func GetGoogleUserInfo(accessToken string) (*GoogleUserInfo, error) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file: ", err)
-	}
-	userInfoURL := os.Getenv("USERINFO")
-
-	client := AppConfig.GoogleLoginConfig.Client(context.Background(), &oauth2.Token{AccessToken: accessToken})
-	resp, err := client.Get(userInfoURL)
-
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("failed to fetch user info: " + resp.Status)
-	}
-
-	var userInfo GoogleUserInfo
-	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
-		return nil, err
-	}
-
-	return &userInfo, nil
 }
