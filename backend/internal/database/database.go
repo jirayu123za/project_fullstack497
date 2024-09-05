9f7395ca55b8de8f4ef1b6d93a743ab156c4a355
jirayu <jirayu004za@gmail.com>
2024-09-05 21:05:27 +0700

    init

---
+++ backend/internal/database/database.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"backend_fullstack/internal/config"
+	"backend_fullstack/internal/models"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func ConnectPostgres(migrate bool) *gorm.DB {
+	config.LoadEnv()
+	dsn := os.Getenv("DATABASE_DSN")
+
+	// Config log
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	// Connect to database
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connected to database successfully")
+
+	// Migration
+	if migrate {
+		err := db.AutoMigrate(
+			&models.UserGroup{},
+			&models.User{},
+			&models.Course{},
+			&models.Assignment{},
+			&models.AssignmentFile{},
+			&models.Enrollment{},
+			&models.InstructorList{},
+			&models.Submission{},
+			&models.Upload{})
+		if err != nil {
+			log.Fatal("Failed to migrate database: ", err)
+		}
+		fmt.Println("Database migration completed!")
+	}
+	return db
+}
