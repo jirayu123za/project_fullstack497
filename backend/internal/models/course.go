1ff44f5445d9ff16494dc36042f8a11ef583b009
jirayu <jirayu004za@gmail.com>
2024-09-06 12:13:48 +0700

    update course_id from string to uuid and create func create uuid before create tb

--- backend/internal/models/course.go
+++ backend/internal/models/course.go
@@ -3,18 +3,26 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Course struct {
-	CourseID          string           `gorm:"primaryKey;autoIncrement"`
-	CourseName        string           `gorm:"type:varchar(255);not null"`
-	CourseDescription string           `gorm:"type:varchar(255)"`
-	Term              string           `gorm:"type:varchar(50);not null"`
+	CourseID          uuid.UUID        `gorm:"primaryKey" json:"course_id"`
+	CourseName        string           `gorm:"type:varchar(255);not null" json:"course_name"`
+	CourseDescription string           `gorm:"type:varchar(255)" json:"course_description"`
+	Term              string           `gorm:"type:varchar(50);not null" json:"term"`
 	Assignments       []Assignment     `gorm:"foreignKey:CourseID"`
 	InstructorLists   []InstructorList `gorm:"foreignKey:CourseID"`
 	Enrollments       []Enrollment     `gorm:"foreignKey:CourseID"`
 	CreatedAt         time.Time
 	UpdatedAt         time.Time
 	DeletedAt         gorm.DeletedAt `gorm:"index"`
+}
+
+func (course *Course) BeforeCreate(tx *gorm.DB) (err error) {
+	if course.CourseID == uuid.Nil {
+		course.CourseID = uuid.New()
+	}
+	return
 }
