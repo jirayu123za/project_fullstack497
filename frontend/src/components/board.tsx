f052e284ab4a045b44eb4db4a7e42733a911410f
pulom999 <pulom999@gmail.com>
2024-09-14 18:41:09 +0700

    addBoard

---
+++ frontend/src/components/board.tsx
@@ -0,0 +1,20 @@
+// src/components/board.tsx
+import React from "react";
+import { FaChild } from "react-icons/fa"; 
+
+interface BoardProps {
+  name: string;
+}
+
+const Board: React.FC<BoardProps> = ({ name }) => {
+  return (
+    <div className="flex items-center p-4 h-[900px] flex-col justify-start">
+      <div className="flex items-center w-full mb-2 border-b">
+        <FaChild className="text-black-500 mr-2" size={20} />
+        <span className="text-gray-800">Welcome, {name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
