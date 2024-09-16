756c8f3e0f062f5e20835f9be8fa31768d9b4c88
pulom999 <pulom999@gmail.com>
2024-09-16 14:03:01 +0700

    ีupdate Viteconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, HttpProxy, ProxyOptions } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const configureFn = (proxy: HttpProxy.Server, _options: ProxyOptions) => {
+  proxy.on("error", (err, _req, _res) => {
+    console.log("proxy error", err);
+  });
+  proxy.on("proxyReq", (_proxyReq, req, _res) => {
+    console.log("Sending Request to the Target:", req.method, req.url);
+  });
+  proxy.on("proxyRes", (proxyRes, req, _res) => {
+    console.log(
+      "Received Response from the Target:",
+      proxyRes.statusCode,
+      req.url
+    );
+  });
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // https://stackoverflow.com/a/74430384
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: configureFn,
+      },
+      "/callback": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: configureFn,
+      },
+    },
+  },
+});
