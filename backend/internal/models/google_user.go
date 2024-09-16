dfd720a465a4e2da5ee4f7986fede5774f1713de
jirayu <jirayu004za@gmail.com>
2024-09-17 02:28:45 +0700

    update struct

--- backend/internal/models/google_user.go
+++ backend/internal/models/google_user.go
@@ -1,8 +1,9 @@
 package models
 
 type GoogleUserInfo struct {
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	GivenName string `json:"given_name"`
-	Picture   string `json:"picture"`
+	Email      string `json:"email"`
+	Name       string `json:"name"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Picture    string `json:"picture"`
 }
