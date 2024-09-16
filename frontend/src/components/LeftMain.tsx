7d60620a770da2334a7699d4bb48aea86c9d4d25
pulom999 <pulom999@gmail.com>
2024-09-16 15:03:24 +0700

    update LeftMain

--- frontend/src/components/LeftMain.tsx
+++ frontend/src/components/LeftMain.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { FaChild } from "react-icons/fa";
+import { IconType } from "react-icons"; 
 
-interface BoardProps {
-  name: string;
+interface HeaderProps {
+  icon: IconType; 
+  title: string; 
 }
 
-const LeftMain: React.FC<BoardProps> = ({ name }) => {
+const LeftMain: React.FC<HeaderProps> = ({ icon: Icon, title }) => {
   return (
     <div className="flex items-center p-4 flex-col justify-start">
       <div className="flex items-center w-full mb-2 border-b py-4  border-B1 font-poppins font-medium text-[32px] text-E1">
-        <FaChild className=" mr-2" size={50} />
-        <span>Welcome, {name}</span>
+      <Icon className="mr-2 text-lg text-344B59 " size={24} />
+
+      <h2 className="text-lg font-semibold text-344B59">{title}</h2>
       </div>
     </div>
   );
