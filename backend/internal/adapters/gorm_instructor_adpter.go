b4d6ee5844846433e02ebde381e3305df95984eb
jirayu <jirayu004za@gmail.com>
2024-09-17 02:25:09 +0700

    add GormInstructorRepository FindCourseByUserID, FindNameByUserID, FindUserGroupByUserID, FindAssignmentByUserID, AddAssignment

--- backend/internal/adapters/gorm_instructor_adpter.go
+++ backend/internal/adapters/gorm_instructor_adpter.go
@@ -52,7 +52,7 @@ func (r *GormInstructorRepository) ModifyCourse(Course *models.Course) error {
 	}
 
 	existingCourse.CourseName = Course.CourseName
-	existingCourse.CourseDescription = Course.CourseDescription
+	//existingCourse.CourseDescription = Course.CourseDescription
 
 	if result := r.db.Save(&existingCourse); result.Error != nil {
 		return result.Error
@@ -68,6 +68,66 @@ func (r *GormInstructorRepository) RemoveCourse(Course *models.Course) error {
 
 	if result := r.db.Delete(&findCourse); result.Error != nil {
 		return result.Error
+	}
+	return nil
+}
+
+// Using jwt
+func (r *GormInstructorRepository) FindCourseByUserID(UserID uuid.UUID) ([]*models.Course, error) {
+	var courses []*models.Course
+	if err := r.db.
+		Joins("JOIN instructor_lists ON instructor_lists.course_id = courses.course_id").
+		Where("instructor_lists.user_id = ?", UserID).
+		Preload("Assignments").
+		Find(&courses).Error; err != nil {
+		return nil, err
+	}
+	return courses, nil
+}
+
+func (r *GormInstructorRepository) FindNameByUserID(userID uuid.UUID) (string, error) {
+	var user models.User
+	if result := r.db.First(&user, "user_id = ?", userID); result.Error != nil {
+		return "", result.Error
+	}
+	return user.FirstName + " " + user.LastName, nil
+}
+
+func (r *GormInstructorRepository) FindUserGroupByUserID(userID uuid.UUID) (string, error) {
+	var groupName string
+	if result := r.db.
+		Table("users").
+		Select("user_groups.group_name").
+		Joins("JOIN user_groups ON user_groups.group_id = users.group_id").
+		Where("users.user_id = ?", userID).
+		Scan(&groupName); result.Error != nil {
+		return "", result.Error
+	}
+	return groupName, nil
+}
+
+func (r *GormInstructorRepository) FindAssignmentByUserID(userID uuid.UUID) ([]*models.Assignment, error) {
+	var assignments []*models.Assignment
+	if err := r.db.
+		Joins("JOIN courses ON courses.course_id = assignments.course_id").
+		Joins("JOIN instructor_lists ON instructor_lists.course_id = courses.course_id").
+		Where("instructor_lists.user_id = ?", userID).
+		Find(&assignments).Error; err != nil {
+		return nil, err
+	}
+	return assignments, nil
+}
+
+// Under line here be GormInstructorRepository of Instructor assignments
+func (r *GormInstructorRepository) AddAssignment(CourseID uuid.UUID, Assignment *models.Assignment) error {
+	// Implement the logic to AddAssignment to the database using GORM.
+	var findCourse *models.Course
+	if result := r.db.First(&findCourse, "course_id = ?", CourseID); result.Error != nil {
+		return result.Error
+	}
+	Assignment.CourseID = CourseID
+	if assignment := r.db.Create(Assignment); assignment.Error != nil {
+		return assignment.Error
 	}
 	return nil
 }
