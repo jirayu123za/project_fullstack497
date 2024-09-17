1732a5bcb0080f30ea499f8254941800d9de4dd5
jirayu <jirayu004za@gmail.com>
2024-09-18 00:40:59 +0700

    add repo FindAssignmentByAssignmentID, FindAssignments, FindAssignmentsByCourseID, ModifyAssignment, RemoveAssignment

--- backend/internal/core/repositories/instructor_repository.go
+++ backend/internal/core/repositories/instructor_repository.go
@@ -23,6 +23,11 @@ type InstructorRepository interface {
 
 	// CRUD operations for Assignments
 	AddAssignment(CourseID uuid.UUID, Assignment *models.Assignment) error
+	FindAssignmentByAssignmentID(AssignmentID uuid.UUID) (*models.Assignment, error)
+	FindAssignments() ([]*models.Assignment, error)
+	FindAssignmentsByCourseID(CourseID uuid.UUID) ([]*models.Assignment, error)
+	ModifyAssignment(Assignment *models.Assignment) error
+	RemoveAssignment(AssignmentID uuid.UUID) error
 
 	// CRD operations for Instructor lists
 	AddInstructorList(CourseID uuid.UUID, InstructorList *models.InstructorList) error  // Create a new instructor list
