31850ada9e6b89e2b9a61ceca0d83cdd39f08a47
jirayu <jirayu004za@gmail.com>
2024-09-18 00:42:11 +0700

    impl http instructor handle GetAssignmentByAssignmentID, GetAssignments, GetAssignmentsByCourseID, UpdateAssignment, DeleteAssignment

--- backend/internal/adapters/http_instructor_adpter.go
+++ backend/internal/adapters/http_instructor_adpter.go
@@ -379,6 +379,141 @@ func (h *HttpInstructorHandler) CreateAssignment(c *fiber.Ctx) error {
 	})
 }
 
+func (h HttpInstructorHandler) GetAssignmentByAssignmentID(c *fiber.Ctx) error {
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment, err := h.services.GetAssignmentByAssignmentID(assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignment by assignment ID",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":    "Assignment found",
+		"assignment": assignment,
+	})
+}
+
+func (h HttpInstructorHandler) GetAssignments(c *fiber.Ctx) error {
+	assignments, err := h.services.GetAssignments()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignments",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":     "Assignments found",
+		"assignments": assignments,
+	})
+}
+
+func (h *HttpInstructorHandler) GetAssignmentsByCourseID(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignments, err := h.services.GetAssignmentsByCourseID(courseID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignments by course ID",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":     "Assignments found",
+		"assignments": assignments,
+	})
+}
+
+func (h *HttpInstructorHandler) UpdateAssignment(c *fiber.Ctx) error {
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment, err := h.services.GetAssignmentByAssignmentID(assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	newAssignment := new(models.Assignment)
+	if err := c.BodyParser(&newAssignment); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Failed to parse request body",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment.AssignmentName = newAssignment.AssignmentName
+	assignment.AssignmentDescription = newAssignment.AssignmentDescription
+	assignment.DueDate = newAssignment.DueDate
+
+	if err := h.services.UpdateAssignment(assignment); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to update assignment",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":    "assignment is updated",
+		"assignment": assignment,
+	})
+}
+
+func (h *HttpInstructorHandler) DeleteAssignment(c *fiber.Ctx) error {
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment, err := h.services.GetAssignmentByAssignmentID(assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	err = h.services.DeleteAssignment(assignment.AssignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to delete assignment",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Assignment is deleted",
+	})
+}
+
 // Under line here be HttpInstructorHandler of Instructor list
 func (h *HttpInstructorHandler) CreateInstructorList(c *fiber.Ctx) error {
 	courseIDParam := c.Query("course_id")
