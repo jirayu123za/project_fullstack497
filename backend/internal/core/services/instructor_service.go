edc08297e194ef3062acd33738c40902a53b9a92
jirayu <jirayu004za@gmail.com>
2024-09-18 00:41:45 +0700

    impl services GetAssignmentByAssignmentID, GetAssignments, GetAssignmentsByCourseID, UpdateAssignment, DeleteAssignment

--- backend/internal/core/services/instructor_service.go
+++ backend/internal/core/services/instructor_service.go
@@ -24,6 +24,11 @@ type InstructorService interface {
 
 	// CRUD operations for Courses
 	CreateAssignment(CourseID uuid.UUID, Assignment *models.Assignment) error
+	GetAssignmentByAssignmentID(AssignmentID uuid.UUID) (*models.Assignment, error)
+	GetAssignments() ([]*models.Assignment, error)
+	GetAssignmentsByCourseID(CourseID uuid.UUID) ([]*models.Assignment, error)
+	UpdateAssignment(Assignment *models.Assignment) error
+	DeleteAssignment(AssignmentID uuid.UUID) error
 
 	// CRD operations for Instructor lists
 	CreateInstructorList(CourseID uuid.UUID, InstructorList *models.InstructorList) error
@@ -135,6 +140,59 @@ func (s *InstructorServiceImpl) CreateAssignment(CourseID uuid.UUID, Assignment 
 	}
 
 	if err := s.repo.AddAssignment(existingCourse.CourseID, Assignment); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *InstructorServiceImpl) GetAssignmentByAssignmentID(AssignmentID uuid.UUID) (*models.Assignment, error) {
+	assignment, err := s.repo.FindAssignmentByAssignmentID(AssignmentID)
+	if err != nil {
+		return nil, err
+	}
+	return assignment, nil
+}
+
+func (s *InstructorServiceImpl) GetAssignments() ([]*models.Assignment, error) {
+	Assignments, err := s.repo.FindAssignments()
+	if err != nil {
+		return nil, err
+	}
+	return Assignments, nil
+}
+
+func (s *InstructorServiceImpl) GetAssignmentsByCourseID(CourseID uuid.UUID) ([]*models.Assignment, error) {
+	Assignments, err := s.repo.FindAssignmentsByCourseID(CourseID)
+	if err != nil {
+		return nil, err
+	}
+	return Assignments, nil
+}
+
+func (s *InstructorServiceImpl) UpdateAssignment(Assignment *models.Assignment) error {
+	existingAssignment, err := s.repo.FindAssignmentByAssignmentID(Assignment.AssignmentID)
+	if err != nil {
+		return err
+	}
+
+	existingAssignment.AssignmentName = Assignment.AssignmentName
+	existingAssignment.AssignmentDescription = Assignment.AssignmentDescription
+	existingAssignment.DueDate = Assignment.DueDate
+
+	if err := s.repo.ModifyAssignment(existingAssignment); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *InstructorServiceImpl) DeleteAssignment(AssignmentID uuid.UUID) error {
+	existingAssignment, err := s.repo.FindAssignmentByAssignmentID(AssignmentID)
+	if err != nil {
+		return err
+	}
+
+	if err := s.repo.RemoveAssignment(existingAssignment.AssignmentID); err != nil {
 		return err
 	}
 	return nil
