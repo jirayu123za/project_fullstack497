a08511169fd4efe3fcd943937aaf98259341bc92
jirayu <jirayu004za@gmail.com>
2024-09-29 04:50:20 +0700

    add repo FindUserIDByEmail

--- backend/internal/core/repositories/user_repository.go
+++ backend/internal/core/repositories/user_repository.go
@@ -11,6 +11,7 @@ type UserRepository interface {
 	Register(user *models.User) error
 	FindUserByID(userID uuid.UUID) (*models.User, error)
 	FindUserByUserName(userName string) (*models.User, error)
+	FindUserIDByEmail(email string) (uuid.UUID, error)
 	FindUsers() ([]*models.User, error)
 	ModifyUser(user *models.User) error
 	RemoveUser(user *models.User) error
