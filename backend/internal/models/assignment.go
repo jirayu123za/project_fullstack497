7a6121e6ef4302ca8806e031c13a4248a5d2f8a5
jirayu <jirayu004za@gmail.com>
2024-09-29 02:28:53 +0700

    update format DueDate yyyy-mm-dd

--- backend/internal/models/assignment.go
+++ backend/internal/models/assignment.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
@@ -12,7 +13,7 @@ type Assignment struct {
 	CourseID              uuid.UUID        `gorm:"not null" json:"course_id"`
 	AssignmentName        string           `gorm:"type:varchar(255);not null" json:"assignment_name"`
 	AssignmentDescription string           `gorm:"type:varchar(255)" json:"assignment_description"`
-	DueDate               time.Time        `gorm:"not null" json:"due_date"`
+	DueDate               time.Time        `gorm:"type:date;not null" json:"due_date"`
 	AssignmentFiles       []AssignmentFile `gorm:"foreignKey:AssignmentID"`
 	Submissions           []Submission     `gorm:"foreignKey:AssignmentID"`
 	CreatedAt             time.Time
@@ -25,4 +26,40 @@ func (assignment *Assignment) BeforeCreate(tx *gorm.DB) (err error) {
 		assignment.AssignmentID = uuid.New()
 	}
 	return
+}
+
+// UnmarshalJSON to handle the date fields in "yyyy-mm-dd" format
+func (assignment *Assignment) UnmarshalJSON(data []byte) error {
+	type Alias Assignment
+	aux := &struct {
+		DueDate string `json:"due_date"`
+		*Alias
+	}{
+		Alias: (*Alias)(assignment),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// Mapping of date strings to assignment's date fields
+	dateFields := []struct {
+		dateStr string
+		target  *time.Time
+	}{
+		{aux.DueDate, &assignment.DueDate},
+	}
+
+	// Parse dates and assign to respective fields
+	for _, field := range dateFields {
+		if field.dateStr != "" {
+			parsedDate, err := time.Parse("2006-01-02", field.dateStr)
+			if err != nil {
+				return err
+			}
+			*field.target = parsedDate
+		}
+	}
+
+	return nil
 }
