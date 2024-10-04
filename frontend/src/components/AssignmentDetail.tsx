a5ced2a9c07177d40d905aa5d5629a9dd2f95f7a
jirayu <jirayu004za@gmail.com>
2024-10-05 00:13:05 +0700

    update component to correctly get props

--- frontend/src/components/AssignmentDetail.tsx
+++ frontend/src/components/AssignmentDetail.tsx
@@ -1,14 +1,14 @@
 
 interface AssignmentDetailProps {
-  role: string;
+  user_group_name: string;
 }
 
-export default function AssignmentDetail({ role }: AssignmentDetailProps) {
+export default function AssignmentDetail({ user_group_name }: AssignmentDetailProps) {
   return (
     <textarea
       className="container border-2 border-B1 min-w-full min-h-[500px] rounded-lg p-6 text-M1"
       placeholder="Assignment details..."
-      disabled={role === "student"}
+      disabled={user_group_name === "STUDENT" || user_group_name === ""}
     />
   );
 }
