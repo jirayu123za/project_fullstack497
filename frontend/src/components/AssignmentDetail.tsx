cf7dd7e3a83c09615f1a19d51320a2387d91930d
jirayu <jirayu004za@gmail.com>
2024-10-06 03:10:37 +0700

    ีupdate AssignmentDetail component receive assignment_desc prop and modify attribute on text area

--- frontend/src/components/AssignmentDetail.tsx
+++ frontend/src/components/AssignmentDetail.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 
 interface AssignmentDetailProps {
   user_group_name: string;
+  assignment_description: string;
 }
 
-export default function AssignmentDetail({ user_group_name }: AssignmentDetailProps) {
+export default function AssignmentDetail({ user_group_name, assignment_description }: AssignmentDetailProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.trim() === "") {
+      setIsFocused(false);
+    }
+  };
+
+
   return (
     <textarea
       className="container border-2 border-B1 min-w-full min-h-[500px] rounded-lg p-6 text-M1"
-      placeholder="Assignment details..."
-      disabled={user_group_name === "STUDENT" || user_group_name === ""}
+      placeholder={!isFocused ? "Assignment details..." : ""}
+      defaultValue={isFocused ? assignment_description : assignment_description}
+      readOnly={user_group_name === "STUDENT" || user_group_name === ""}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     />
   );
 }
