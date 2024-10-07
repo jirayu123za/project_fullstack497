15a3d634fc29e49042928ccfdf1c54cd37f3e4af
jirayu <jirayu004za@gmail.com>
2024-10-08 05:50:20 +0700

    init storage

---
+++ backend/internal/storage/storage.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func MinioConnection() (*minio.Client, error) {
+	ctx := context.Background()
+	endpoint := os.Getenv("MINIO_ENDPOINT")
+	accessKeyID := os.Getenv("MINIO_ACCESS_KEY")
+	secretAccessKey := os.Getenv("MINIO_SECRET_KEY")
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, errInit := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if errInit != nil {
+		log.Fatalln(errInit)
+	}
+
+	// Make a new bucket called dev-minio.
+	bucketName := os.Getenv("MINIO_BUCKET_NAME")
+	location := "ap-southeast-1"
+
+	err := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+	return minioClient, errInit
+}
