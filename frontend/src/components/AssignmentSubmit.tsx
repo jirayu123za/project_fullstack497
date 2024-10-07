46bf9b5e910e14eebdb2b7a9534900150c495883
jirayu <jirayu004za@gmail.com>
2024-10-08 05:48:02 +0700

    down scale body circle

--- frontend/src/components/AssignmentSubmit.tsx
+++ frontend/src/components/AssignmentSubmit.tsx
@@ -10,7 +10,7 @@ export default function AssignmentSubmit({ user_name, user_submitted }: Assignme
     <div className="flex justify-start items-center gap-3 p-2">
       <div>
         {/* วงกลมแสดงสถานะ */}
-        <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="19" height="19" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="14.5" cy="14.5" r="14.5" fill={circleColor} />
         </svg>
       </div>
