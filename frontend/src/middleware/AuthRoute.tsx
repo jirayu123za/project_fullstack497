fa5f6aa29d6e3a597508b8dad8025868d494816c
jirayu <jirayu004za@gmail.com>
2024-10-11 02:04:33 +0700

    update check if exp is expires navigate to /landing

--- frontend/src/middleware/AuthRoute.tsx
+++ frontend/src/middleware/AuthRoute.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 const AuthRoute = ({ children }: { children: JSX.Element }) => {
   const token = Cookies.get('jwt-token');
 
   if (!token) {
+    return <Navigate to="/landing" replace />;
+  }
+
+  try {
+    const decodedToken: { exp: number } = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+
+    if (decodedToken.exp < currentTime) {
+      return <Navigate to="/landing" replace />;
+    }
+  } catch (error) {
     return <Navigate to="/landing" replace />;
   }
 
